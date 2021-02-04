123db03a8f07592354a5e8a5bd041d7d731aac2a
Kevin <kevingleijm@gmail.com>
2021-02-04 13:46:08 +0100

    implemented acting on arguments(untested)

--- venv/videophyCommandLine.py
+++ venv/videophyCommandLine.py
@@ -2,6 +2,7 @@
 # import needed libraries
 import sys
 import os
+import shutil
 # install open-cv if not yet installed
 os.system("pip install opencv-python")
 from cv2 import cv2
@@ -10,33 +11,33 @@ from cv2 import cv2
 print('Number of arguments: {}'.format(len(sys.argv)))
 
 # read absolute path from arguments
-
-# generate absolute path
-dirPath = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/") + "/testImages/"
+dirPath = sys.argv[1]
+title = sys.argv[2]
+fpsInput = int(sys.argv[3])
 print(dirPath)
-pathString = "./testImages/"
+sourcePathString = dirPath + "/source"
 
 # reading in files from that path
-path, dirs, files = next(os.walk(pathString))
+path, dirs, files = next(os.walk(dirPath))
 print(path, dirs, files)
 clip_count = len(files)
 
-# make directory if not exists
+# make sourceImagedirectory if not exists
 try:
-    os.mkdir(dirPath + 'output')
+    os.mkdir(sourcePathString)
 except:
     pass
 
 # loop over all files in folder and store them as cv2 compatible images
 img = []
 for file in files:
-    # print(dirPath + file)
     img.append(cv2.imread(dirPath + file))
+    shutil.move(file, sourcePathString)
 
 # declare open-cv variables
 height, width, layers = img[1].shape
 fourcc = cv2.VideoWriter_fourcc(*'I420')
-video = cv2.VideoWriter(dirPath + 'output/video.avi', fourcc, 10.0, (width, height))
+video = cv2.VideoWriter(dirPath + 'video.avi', fourcc, fpsInput, (width, height))
 
 # write all images to video
 for j in range(clip_count):
@@ -47,4 +48,4 @@ for j in range(clip_count):
 cv2.destroyAllWindows()
 video.release()
 
-print("Released video to: " + dirPath + 'output/video.mp4')
+print("Released video to: " + dirPath)
