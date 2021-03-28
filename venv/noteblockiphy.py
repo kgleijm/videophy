bba6d8022e543698e1fad91bcce831b66f755bed
Kevin <kevingleijm@gmail.com>
2021-03-29 00:52:15 +0200

    added fonts started noteblockiphy

--- venv/noteblockiphy.py
+++ venv/noteblockiphy.py
@@ -9,19 +9,25 @@ from PIL import Image, ImageDraw
 # dirPath = sys.argv[1]
 
 #initialize margins
-noteWidth = 300
-noteHeigth = 500
-ruleSpace = 100
-sideMarging = 75
+noteWidth = 500
+noteHeigth = 700
+ruleSpace = 20
+sideMarging = 50
 
 
 # fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40)
 
-
-out = Image.new("RGB", (noteWidth, noteHeigth), (255, 255, 255))
-
+# initialize image
+out = Image.new("RGBA", (noteWidth, noteHeigth), (255, 255, 255, 255))
 d = ImageDraw.Draw(out)
-d.line((ruleSpace, 0, ruleSpace, noteHeigth), fill=255)
+
+# draw lines
+for i in range(1, (noteHeigth//ruleSpace)):
+    d.line((0, i*ruleSpace, noteWidth, i*ruleSpace), fill=255)
+
+
+# draw margin line
+d.line((sideMarging, 0, sideMarging, noteHeigth), fill=(255, 0, 0, 255))
 
 out.show()
 
