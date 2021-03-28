9549149d6b66251a04592f2aa3bcaa3da0ccad3c
Kevin <kevingleijm@gmail.com>
2021-03-29 01:44:09 +0200

    started implementing ripping

--- venv/noteblockiphy.py
+++ venv/noteblockiphy.py
@@ -1,6 +1,7 @@
 
 import sys
 import os
+import random
 
 os.system("pip install --upgrade Pillow")
 from PIL import Image, ImageDraw, ImageFont
@@ -20,25 +21,20 @@ with open("C:\\Users\\kevin\\PycharmProjects\\videophy\\venv\\textContainer.txt"
 #initialize margins
 noteWidth = 60 + maxWidth*11
 ruleSpace = 20
-noteHeigth = (lines + 2)*ruleSpace
+noteHeigth = (lines + 3)*ruleSpace
 sideMarging = 50
-
-
-
 
 # initialize image
 out = Image.new("RGBA", (noteWidth, noteHeigth), (255, 255, 255, 255))
 d = ImageDraw.Draw(out)
 fnt = ImageFont.truetype(r"C:\Users\kevin\PycharmProjects\videophy\venv\testImages\fonts\PinkChicken-Regular.otf", 25)
 
-
-
 # draw lines
 for i in range(1, (noteHeigth//ruleSpace)):
     d.line((0, i*ruleSpace, noteWidth, i*ruleSpace), fill=255)
 
 # read and draw text from textcontainer
-index = 1
+index = 2
 with open("C:\\Users\\kevin\\PycharmProjects\\videophy\\venv\\textContainer.txt", "r") as a_file:
   for line in a_file:
     #line = line.strip()
@@ -50,6 +46,33 @@ with open("C:\\Users\\kevin\\PycharmProjects\\videophy\\venv\\textContainer.txt"
 
 # draw margin line
 d.line((sideMarging, 0, sideMarging, noteHeigth), fill=(255, 0, 0, 255))
+
+# rip top
+last = ruleSpace/2
+for i in range(out.width):
+    if last <= 0.3*ruleSpace:
+        new = last + random.randint(0,1)
+    elif last >= 0.7*ruleSpace:
+        new = last + random.randint(-1, 0)
+    else:
+        new = last + random.randint(-1, 1)
+    d.line((i, 0, i, new), fill=(0, 255, 0, 0))
+    last = new
+
+# rip bottom
+last = ruleSpace/2
+for i in range(out.width):
+    if last <= 0.3*ruleSpace:
+        new = last + random.randint(0,1)
+    elif last >= 0.7*ruleSpace:
+        new = last + random.randint(-1, 0)
+    else:
+        new = last + random.randint(-1, 1)
+    d.line((i, out.height, i, out.height - new), fill=(255, 0, 0, 0))
+    last = new
+
+
+
 
 out.show()
 
